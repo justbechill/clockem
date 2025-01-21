03173171cb3860953b14f81c00e9284a50f68d63
JustBeChill <63560117+JustBeChill@users.noreply.github.com>
2025-01-21 12:21:57 -0800

    Created base weather script

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod clock;
 mod wallpaper;
+mod weather;
 
-use std::{fs, io::Write, io::Result};
 use gio::prelude::*;
 use serde::Deserialize;
+use std::{fs, io::Result, io::Write};
 
 #[derive(Debug, Deserialize, Clone)]
 struct Clock {
@@ -23,10 +24,20 @@ struct Wallpaper {
     vert_adjustment: Option<i32>,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+struct Weather {
+    enabled: Option<bool>,
+    position_x: Option<i32>,
+    position_y: Option<i32>,
+    y_align: Option<String>,
+    update_interval: Option<u32>,
+}
+
 #[derive(Debug, Deserialize)]
 struct Config {
     clock: Option<Clock>,
     wallpaper: Option<Wallpaper>,
+    weather: Option<Weather>,
 }
 
 /**
@@ -81,10 +92,11 @@ fn load_config() -> Result<Config> {
 
 fn main() -> Result<()> {
     // CREATE CONFIG DIRECTORY IF IT DOESN'T EXIST
-    let dir_path = std::env::var("HOME").expect("Could not get home directory.") + "/.config/clockem";
+    let dir_path =
+        std::env::var("HOME").expect("Could not get home directory.") + "/.config/clockem";
     if !path_exists(&dir_path) {
         match fs::create_dir(&dir_path) {
-            Ok(_text) => {},
+            Ok(_text) => {}
             Err(e) => {
                 log::warn!("Could not create config directory: {}", e)
             }
@@ -94,7 +106,7 @@ fn main() -> Result<()> {
     let application = gtk4::Application::new(Some("com.clockem"), Default::default());
     let config = load_config()?;
 
-    if let Some(wallpaper) = config.wallpaper{
+    if let Some(wallpaper) = config.wallpaper {
         if wallpaper.enabled.unwrap_or(false) {
             application.connect_activate(move |app| {
                 let _ = load_css();
@@ -103,11 +115,20 @@ fn main() -> Result<()> {
         }
     }
 
-    if let Some(clock) = config.clock{
+    if let Some(clock) = config.clock {
         if clock.enabled.unwrap_or(false) {
             application.connect_activate(move |app| {
                 let _ = load_css();
                 crate::clock::build(app, clock.clone());
+            });
+        }
+    }
+
+    if let Some(weather) = config.weather {
+        if weather.enabled.unwrap_or(false) {
+            application.connect_activate(move |app| {
+                let _ = load_css();
+                crate::weather::build(app, weather.clone());
             });
         }
     }
---
+++ src/weather.rs
@@ -0,0 +1,61 @@
+use crate::Weather;
+use gtk4::{prelude::*, Align};
+use gtk4_layer_shell::{Edge, Layer, LayerShell};
+
+pub fn build(application: &gtk4::Application, weather_config: Weather) {
+    // SET UP WINDOW AS A LAYER
+    let weather_window = gtk4::ApplicationWindow::new(application);
+    weather_window.init_layer_shell();
+    weather_window.set_layer(Layer::Background);
+    weather_window.set_namespace("clockem-weather");
+
+    // LABEL STUFFS
+    let container = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
+
+    let top = gtk4::Label::new(Some("Weather"));
+    let bottom = gtk4::Label::new(Some(""));
+
+    top.add_css_class("weather-top");
+    bottom.add_css_class("weather-bottom");
+
+    container.append(&top);
+    container.append(&bottom);
+
+    // WINDOW FORMATTING
+    weather_window.set_title(Some("clockem-weather"));
+
+    let position_x = weather_config.position_x.unwrap_or(0);
+    let position_y = weather_config.position_y.unwrap_or(0);
+
+    // TESTING WINDOW POSITION STUFFS
+    let mut anchors = Vec::new();
+
+    if let Some(s) = weather_config.y_align {
+        match s.as_str() {
+            "center" => {
+                anchors.push((Edge::Left, true));
+                container.set_valign(Align::Center)
+            }
+            "bottom" => {
+                anchors.push((Edge::Left, true));
+                anchors.push((Edge::Bottom, true));
+            }
+            _ => {
+                anchors.push((Edge::Left, true));
+                anchors.push((Edge::Top, true));
+            }
+        }
+    }
+
+    for (anchor, state) in anchors {
+        weather_window.set_anchor(anchor, state);
+    }
+
+    weather_window.set_margin(Edge::Left, position_x);
+    weather_window.set_margin(Edge::Top, position_y);
+
+    // SHOW WEATHER
+    weather_window.set_child(Some(&container));
+    weather_window.show();
+
+}
